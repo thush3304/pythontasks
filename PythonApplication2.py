857dbd2227af2ef23a45bed6c0597cb48046bcf1
Thushith Premadas <86304577+thush3304@users.noreply.github.com>
2021-07-05 16:24:32 +0100

    QA grade task

---
+++ PythonApplication2.py
@@ -0,0 +1,17 @@
+n=0
+mark=int(input("Enter your mark:"))
+if mark>85:
+    print("Distinction")
+elif 65<mark<85:
+    print("pass")
+else:
+    print("fail")
+count=0
+while count<5:
+    count=count + 1
+    name=input("Whats your name?")
+    print(name + " is awesome!")
+for i in range(10,21,3):
+    print(i)
+
+    
